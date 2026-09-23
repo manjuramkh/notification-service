7fe1b139a40e98d2bd4d86167f563ffb6d3365ac
manjuramkh <62883982+manjuramkh@users.noreply.github.com>
2026-09-23 14:25:35 +0530

    Delete src/main/java/com/bank/infrastructure directory

--- src/main/java/com/bank/infrastructure/kafka/KafkaTopics.java
+++
@@ -1,9 +0,0 @@
-package com.bank.infrastructure.kafka;
-
-public final class KafkaTopics {
-
-    private KafkaTopics() {
-    }
-
-    public static final String PAYMENT_EVENTS = "payment.events";
-}
